aa6181d99c68ab2b0649620c5c4a9ffb0f61d56c
Keith Babo <kbabo@redhat.com>
2015-09-23 13:35:02 -0400

    support in common

--- transformation/common/src/test/java/org/jboss/tools/fuse/transformation/model/ModelBuilderTest.java
+++ transformation/common/src/test/java/org/jboss/tools/fuse/transformation/model/ModelBuilderTest.java
@@ -21,6 +21,7 @@ import java.util.List;
 
 import junit.framework.Assert;
 
+import org.jboss.tools.fuse.transformation.model.ModelBuilder.Strategy;
 import org.junit.Test;
 
 public class ModelBuilderTest {
@@ -72,12 +73,26 @@ public class ModelBuilderTest {
         Model model = ModelBuilder.fromJavaClass(ListOfStringsAndNumbers.class);
         Assert.assertEquals(3, model.listFields().size());
     }
+    
+    @Test
+    public void listsOfStringsAndNumbersProperties() throws Exception {
+        Model model = ModelBuilder.fromJavaClass(ListOfStringsAndNumbers.class, Strategy.PROPERTIES);
+        Assert.assertEquals(2, model.listFields().size());
+    }
 }
 
 class ListOfStringsAndNumbers {
     private List<Number> numbers;
     private List<String> strings;
     private String field1;
+    
+    public List<Number> getNumbers() {
+        return numbers;
+    }
+    
+    public List<String> getStrings() {
+        return strings;
+    }
 }
 
 class NoSuper {
